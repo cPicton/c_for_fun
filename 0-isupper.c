8ed7a2f9e79cdade158afb4b6c6030592b2df83e
Chris Picton <pictonchris2@gmail.com>
2021-09-02 13:25:30 -0700

    is upper

---
+++ 0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * int _isupper -  checks for uppercase character.
+ * @c: character to be checked
+ * Return: 1 if true, 0 if false
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+
+else
+{
+return (0);   
+}
+ 
+}
