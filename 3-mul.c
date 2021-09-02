bef8863cdf5fa5d5927ea90a57d748ca971d636c
Chris Picton <pictonchris2@gmail.com>
2021-09-02 14:03:08 -0700

    multiplication

---
+++ 3-mul.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* int mul - multiplies 2 integers
+ *@a:input
+ *
+ *@b:input
+ *
+ *Description:a * b
+ *
+ *Return:multiplication
+ *
+ */
+
+ int mul(int a, int b)
+
+{
+int multiplication;
+
+multiplication = a * b;
+     
+return (multiplication);
+}
