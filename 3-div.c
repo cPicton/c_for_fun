e39bb876be8dce285ee922c899a5d0a708440572
Chris Picton <pictonchris2@gmail.com>
2021-09-02 14:08:03 -0700

    division

---
+++ 3-div.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* int div - divides 2 integers
+ *@a:input
+ *@b:input
+ *Description:a / b
+ *Return:multiplication
+ */
+
+ int div(int a, int b)
+
+{
+     int division;
+
+     division = a / b;
+     
+     return (division);
+}
--- main.h
+++ main.h
@@ -7,6 +7,8 @@ int _isdigit(int c);
 
 int mul(int a, int b);
 
+int div(int a, int b);
+
 
 
 
